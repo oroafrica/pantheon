e1c8af30a219e88b3175e37b6422d2c5ea386631
oroafrica <moegamat@oroafrica.com>
2022-01-25 08:49:46 +0200

    pf-006

--- js/camlib.js
+++ js/camlib.js
@@ -6,49 +6,131 @@
      * fabricjs 4.6
     */
     const { log, trace, error, warn, info, clear } = console;
+    const btns = $(".btn").toArray();
     /* form variables */
-    const productStyle = $("#camProduct").text();
-    const productCharacter = JSON.parse($("#camCharacteristics").text());
-    const productInventory = JSON.parse($("#camInventory").text());
+    var debug = false;
+
+    var _productStyle = $("#camProduct").text();
+    var _productCharacter = JSON.parse($("#camCharacteristics").text());
+    var productInventory = JSON.parse($("#camInventory").text());
     const _svgSelect = $("._svgInput:text");
     const _alloySelect = $("#alloyInput");
     const _motifSelect = $("#motifInput");
     const _accentSelect = $("#accentInput");
+    var _svgFile;
+    var _svgAlloyImage;
     /* end form variables */
 
     /* canvas variables */
-
+    const config ={ cnf:{ canvasWidth:300 ,canvasHeight:300 ,background:"rgba(231,245,249,0.8)",objectCaching:false,hoverCursor:"pointer" } };
+    var canvas; 
+    var serializer = new XMLSerializer();
+    var doc, svgString, objs;
+    const parser = new DOMParser();
     /* end canvas variables */
 
     const subAction = 
     {
-                
+        toggleCanvas:()=> $(btns[3]).click(()=> $(".canvasHost").toggleClass("d-none"))  
+        ,svgFileUrl:(name="MS016")=> { _svgFile = "./svg/".concat(name).concat(".svg"); return _svgFile; }
+        ,imageFileUrl:(name="MS016")=> { _svgAlloyImage = "./images/".concat(name).concat(".png"); return _svgAlloyImage; }
+        ,loadFabricSvg:()=>{}  
+        ,loadNormalSvg:()=>{} 
+        ,test:()=> $(btns[0]).click( ()=> $("#svgString").attr("src","./svg/MS016.svg") )  //log( new XMLSerializer().serializeToString( $("#svgString").attr("src")));
+        ,debugLog:()=> 
+        {   
+            let report = {};
+            report.productStyle=_productStyle;
+            report.productCharacter=_productCharacter;
+            report.productInventory=productInventory;
+            report.svgString= "uncomment me!"; //svgString;
+
+            if(debug){ log(JSON.stringify(report)); } 
+        }
+        ,drawSvg:(svgString)=>
+        {
+            canvas.clear();
+            fabric.loadSVGFromString(svgString, function(objects, options) 
+            {
+              objs = fabric.util.groupSVGElements(objects, options);
+              objs.set({selectable:false,objectCache:false});
+              canvas.add(objs).renderAll();
+              objs.moveTo(0);
+              objs.center();
+            });
+        } 
+        ,scaleFactor:(val)=>
+        {
+            val = (isNaN(val)) ? 2 : val; 
+            canvas.zoomToPoint(new fabric.Point(canvas.width/2, canvas.height/2), val);
+            subAction.drawImage();
+        }
+        ,drawImage:()=>
+        {
+            let dim = 2000;
+            var tmpCanvas = document.createElement('canvas');
+            tmpCanvas.id="canvas";
+            tmpCanvas.setAttribute("width",dim);
+            tmpCanvas.setAttribute("height",dim);
+            let ctx = tmpCanvas.getContext('2d');
+
+            var im = new Image();
+            im.src = "data:image/svg+xml,".concat(encodeURIComponent(canvas.toSVG()));
+            //draw 
+            im.onload = ()=>{ctx.drawImage(im, 0, 0, dim*window.devicePixelRatio, dim*window.devicePixelRatio); $("#dynamicImage").attr("src",tmpCanvas.toDataURL());}; 
+        }
+        
     };
-    
+
+    /* svg core functions */
     const camGroup =
     {
         PATHS:()=>
         {
-            
+            warn("hitting patsd");
+
         }
         ,NONE:()=>
         {
-            alert("no svg handler configured");
+
+           warn("no svg handler configured");
         }
     };
     
     /* method return object */
-    log(productStyle);
     let functions = (camStyle = "NONE")=>
     {
-        let action = camGroup[camStyle];
-        return action();
-//        var result = 
-//        {
-//            classic:()=>{return "classic names"; }
-//            ,retro:()=>{return "retro names"; }
-//        };
-//        return result;
+
+        subAction.toggleCanvas();
+        // let s =  JSON.parse($("#camCharacteristics").text()).charac_camCode;
+        //log(s); 
+        // let k = s+"888";
+        // log(k);
+        // log(camGroup[k]);
+        _productCharacter = JSON.parse($("#camCharacteristics").text()).charac_camCode;
+        log(typeof _productCharacter);
+        var s = "PATHS";
+        let action = camGroup[s];
+
+        /* init canvas */
+        fetch(subAction.svgFileUrl(_productStyle),{method:"GET"})
+        .then(resp=> (resp.status === 200) ? resp.text() : resp.status)
+        .then(d=> 
+        {
+            canvas = new fabric.Canvas("orderCanvas",config); 
+            doc = parser.parseFromString(d,"image/svg+xml");
+            svgString = serializer.serializeToString(doc);
+
+            //subAction.test();
+            subAction.debugLog();
+            subAction.drawSvg(svgString);
+            subAction.scaleFactor(3);
+
+            return  action();
+
+        });
+
+        // return action();
     };
     
     /* assign method if undefined. global name: svgInput, call: svgInput.classic() */ 
